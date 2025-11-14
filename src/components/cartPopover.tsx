3c156fb7aae088719fd0bd61d74b9bca5425160f
Baseer Afridi <githubbs7@gamil.com>
2025-11-15 00:37:09 +0500

    cart icon added

--- src/components/cartPopover.tsx
+++ src/components/cartPopover.tsx
@@ -1,33 +1,36 @@
 import { Button } from "@/components/ui/button"
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
 } from "@/components/ui/popover"
 import CartLogic from "@/lib/cartLogic";
 import CartComponent from "./cartComponent";
+import { IoCartOutline, IoCartSharp } from "react-icons/io5";
 
-export  default async function PopoverCart() {
+export default async function PopoverCart() {
     const cartData = await CartLogic()
 
     if (!cartData) {
         return <div className="cart-empty">Your cart is empty!</div>;
     }
 
-  return (
-    <main className="absolute right-5 bottom-5">
-        <Popover>
-      <PopoverTrigger asChild>
-        <Button variant="outline" className="text-black">
-            <IoCartOutline />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-80">
-        <CartComponent cartResponse={cartData} />
-      </PopoverContent>
-    </Popover>
-    </main>
-  )
+    return (
+        <main className="absolute right-5 bottom-5">
+            <Popover>
+                <PopoverTrigger asChild>
+                    <Button variant="outline" className="text-black">
+                        {cartData ? <IoCartSharp /> :
+                            <IoCartOutline className="text-xl" />
+                        }
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-80">
+                    <CartComponent cartResponse={cartData} />
+                </PopoverContent>
+            </Popover>
+        </main>
+    )
 }
 
 // add popover icon from lucide or similar package
